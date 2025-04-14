a5091abfe9b0cbe6e8119ea9dd0148c2402a3600
Vilazza1 <vinicius.vilaca2023@gmail.com>
2025-04-14 14:56:19 -0300

    Aprimorando exibição data

--- 3782-javascript-projeto-base/js/ui.js
+++ 3782-javascript-projeto-base/js/ui.js
@@ -6,7 +6,10 @@ const ui = {
     document.getElementById("pensamento-id").value = pensamento.id;
     document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
     document.getElementById("pensamento-autoria").value = pensamento.autoria;
-    document.getElementById("pensamento-data").value = pensamento.data;
+    document.getElementById("pensamento-data").value = pensamento.data
+      .toISOString()
+      .split("T")[0];
+    document.getElementById("form-container").scrollIntoView();
   },
 
   limparFormulario() {
@@ -58,7 +61,14 @@ const ui = {
     pensamentoAutoria.classList.add("pensamento-autoria");
 
     const pensamentoData = document.createElement("div");
-    const dataFormatada = pensamento.data.toLocaleDateString('pt-BR')
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    };
+    const dataFormatada = pensamento.data.toLocaleDateString("pt-BR", options);
     pensamentoData.textContent = dataFormatada;
     pensamentoData.classList.add("pensamento-data");
 
