d036b90268b78bd76f2e660becb4b80266d1b6e0
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-08-27 00:39:12 -0300

    Eventos

--- React/organo/src/componentes/Formulario/index.js
+++ React/organo/src/componentes/Formulario/index.js
@@ -15,14 +15,19 @@ const Formulario = () => {
   'Inovação e Gestão'
 ]
 
+const aoSalvar = (evento) => {
+  evento.preventDefault()
+  console.log('Form foi submetido')
+}
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times}/>
+        <CampoTexto obrigadorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigadorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigadorio={true} label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigadorio={true} label="Time" itens={times}/>
         <Botao>
           Criar Card
         </Botao>
