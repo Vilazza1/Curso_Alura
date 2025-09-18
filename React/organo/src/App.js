b36c72557f66d4549832831ea18bebe3f2be7406
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-09-17 22:24:52 -0300

    Criando os times

--- React/organo/src/App.js
+++ React/organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 
 function App() {
@@ -16,6 +17,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => novoColadorador(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="UX e Desing"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
