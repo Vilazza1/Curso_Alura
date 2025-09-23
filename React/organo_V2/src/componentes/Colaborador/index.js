4dc94a52bb30a7370d84f78fb0cb15e6ebda4027
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-09-22 23:32:22 -0300

    importando o icon

--- React/organo_V2/src/componentes/Colaborador/index.js
+++ React/organo_V2/src/componentes/Colaborador/index.js
@@ -1,8 +1,9 @@
+import { IoMdCloseCircle } from "react-icons/io";
 import './colaborador.css'
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
-        <div className="deletar" onClick={aoDeletar}>Deletar</div>
+        <IoMdCloseCircle size={25} className="deletar" onClick={aoDeletar} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
