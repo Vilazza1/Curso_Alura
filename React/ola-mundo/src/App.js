66b95a5c54d05aed3a802549efb13a73c9e205df
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-09-29 22:28:37 -0300

    testando as rotas

--- React/ola-mundo/src/App.js
+++ React/ola-mundo/src/App.js
@@ -1,7 +1,14 @@
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/sobreMim";
+
+console.log(window.location);
+
+const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />;
+
 function App() {
   return (
     <div className="App">
-      <h1>Olá, Mundo!</h1>
+      {pagina}
     </div>
   );
 }
