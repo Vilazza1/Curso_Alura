0f3757087854879d5363d21737200516ca104d16
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-09-29 23:26:43 -0300

    Links do react-router-dom

--- React/ola-mundo/src/App.js
+++ React/ola-mundo/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobreMim";
+import Menu from "./componentes/Menu";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
