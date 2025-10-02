a59a26c8a0fee270ad0b6f2f9d9882f3799bdd11
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-01 23:37:55 -0300

    Utilzand  rotas aninhadas

--- React/ola-mundo/src/routes.js
+++ React/ola-mundo/src/routes.js
@@ -3,7 +3,7 @@ import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
-
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
@@ -11,8 +11,10 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
       <Rodape />
