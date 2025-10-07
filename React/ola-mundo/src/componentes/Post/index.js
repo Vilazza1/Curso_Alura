2c2600a86af4bb9de8cb3b0d401fdb51bcae32e6
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-06 23:29:58 -0300

    React MarkDown

--- React/ola-mundo/src/componentes/Post/index.js
+++ React/ola-mundo/src/componentes/Post/index.js
@@ -1,23 +1,27 @@
-import { useParams } from "react-router-dom"
-import posts from "json/posts.json"
+import "./Post.css";
+
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
 
-export default function Post () {
-    const parametros = useParams();
+export default function Post() {
+  const parametros = useParams();
 
-    const post = posts.find((post) => {
-        return post.id === Number(parametros.id);
-    })
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
 
-    
-    return(
-        <div>
-            <PostModelo 
-                fotoCapa={`/assets/posts/${post.id}/capa.png`}
-                titulo={post.titulo}
-            >
-                Texto...
-            </PostModelo>
+  return (
+    <div>
+      <PostModelo
+        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+        titulo={post.titulo}
+      >
+        <div className="post-markdown-container">
+          <ReactMarkdown>{post.texto}</ReactMarkdown>
         </div>
-    )
+      </PostModelo>
+    </div>
+  );
 }
