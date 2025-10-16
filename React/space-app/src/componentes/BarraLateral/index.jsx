58b53244bf5240efb2e6a40659f863915d4634f1
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-15 23:18:55 -0300

    Item de Navegação

--- React/space-app/src/componentes/BarraLateral/index.jsx
+++ React/space-app/src/componentes/BarraLateral/index.jsx
@@ -1,25 +1,32 @@
 import styled from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
   width: 212px;
-
-`
+`;
 
 const BarraLateral = () => {
   return (
     <aside>
       <nav>
-        <ul>
-          <li>
-            <a href="">Inicio</a>
-          </li>
-          <li>
-            <a href="">Mais Vistas</a>
-          </li>
-        </ul>
+        <ListaEstilizada>
+          <ItemNavegacao
+            iconeAtivo="/icones/home-ativo.png"
+            iconeInativo="/icones/home-inativo.png"
+            ativo
+          >
+            Início
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/mais-vistas-ativo.png"
+            iconeInativo="/icones/mais-vistas-inativo.png"
+          >
+            Mais Vistas
+          </ItemNavegacao>
+        </ListaEstilizada>
       </nav>
     </aside>
   );
