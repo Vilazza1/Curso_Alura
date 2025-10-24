6c97647df280639fdca224c1646bedd6e5e49c84
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-23 22:46:23 -0300

    corrigindo as imgaem

--- React/space-app/src/componentes/Galeria/Populares/index.jsx
+++ React/space-app/src/componentes/Galeria/Populares/index.jsx
@@ -1,9 +1,9 @@
 import Titulo from "../../Titulo"
 
 const Populares = () => {
-    return (
+    return <div style={{ minWidth: 212 }}>
         <Titulo $alinhamento="center">Populares</Titulo>
-    )
+    </div>
 }
 
 export default Populares
