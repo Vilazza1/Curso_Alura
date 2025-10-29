17606e1225e4ba47d8075b4583bf69c8557c2988
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-28 22:48:04 -0300

    foto favorita

--- React/space-app/src/componentes/Galeria/index.jsx
+++ React/space-app/src/componentes/Galeria/index.jsx
@@ -20,7 +20,7 @@ const ImagensContainer = styled.section`
     gap: 24px;
 `
 
-const Galeria = ({ fotos = [], aoFotoSelecionada }) => {
+const Galeria = ({ fotos = [], aoFotoSelecionada, aoAlternarFavirito }) => {
     return (
         <>
             <Tags />
@@ -30,6 +30,7 @@ const Galeria = ({ fotos = [], aoFotoSelecionada }) => {
                     <ImagensContainer>
                         {fotos.map(foto => <Imagem 
                             aoZoomSolicitado={aoFotoSelecionada}
+                            aoAlternarFavirito={aoAlternarFavirito}
                             key={foto.id} 
                             foto={foto} />)
                         }
