72df5348ed7f5b8c7deadf358729ac601f1314d6
Vilaza1 <vinicius.vilaca2023@gmail.com>
2025-10-29 23:14:59 -0300

    Context API

---
+++ React/Meteora/src/context/CarrinhoContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const CarrinhoContext = createContext();
+
+export const CarrinhoProvider = ({children}) => {
+    return (
+        <createContext.prototype>
+            {children}
+        </createContext.prototype>
+    )
+}
