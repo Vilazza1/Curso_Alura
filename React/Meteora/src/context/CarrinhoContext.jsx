9660430a8b22f943fcca0b2657e2dd859716cfeb
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-01-05 23:04:14 -0300

    Refatorando o hook useCarrinhoContext

--- React/Meteora/src/context/CarrinhoContext.jsx
+++ React/Meteora/src/context/CarrinhoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
 import { carrinhoReducer } from "../reducers/carrinhoReducers";
 
 export const CarrinhoContext = createContext();
@@ -7,9 +7,27 @@ CarrinhoContext.displayName = "Carrinho";
 const estadoInicial = [];
 
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial );
-  const [quantidade] = useState(0);
-  const [valorTotal] = useState(0);
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (acumulador, produto) => ({
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+      }),
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0,
+      }
+    );
+  }, [carrinho]);
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
 
   return (
     <CarrinhoContext.Provider
