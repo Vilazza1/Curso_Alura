bdb39c7e38e9301b6a502ee471ce15c3fd74c0ee
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-01-12 21:21:36 -0300

    Adiciona estrutura e estilo à página Favoritos com componentes Banner, Titulo e Card

--- React/cinetag/src/pages/Favoritos/index.js
+++ React/cinetag/src/pages/Favoritos/index.js
@@ -1,9 +1,24 @@
-import styles from "./Favoritos.module.css"
+import Banner from "components/Banner";
+import styles from "./Favoritos.module.css";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
 
-function Favoritos () {
-    return(
-        <h1>Ola Mundo</h1>
-    )
+function Favoritos() {
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card
+          id="1"
+          titulo="Gato Fodido"
+          capa="https://thecatapi.com/api/images/get?format=src&type=png"
+        />
+      </section>
+    </>
+  );
 }
 
-export default Favoritos
+export default Favoritos;
