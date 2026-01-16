be913bf32c274be48058a25fa43786ae5b2158eb
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-01-15 22:26:38 -0300

    Adiciona componente Player e integra às rotas da aplicação

---
+++ React/cinetag/src/pages/Player/index.js
@@ -0,0 +1,17 @@
+import Banner from 'components/Banner';
+import styles from './Player.module.css'
+import Titulo from 'components/Titulo';
+
+function Player (){
+    return (
+        <>
+        
+            <Banner img="player"/>
+            <Titulo>
+                <h1>Player</h1>
+            </Titulo>
+        </>
+    )
+}
+
+export default Player;
