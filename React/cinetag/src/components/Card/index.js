bd99ec80400699d5afbf9563d808b2a08e11a794
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-01-15 21:45:12 -0300

    Refatora o componente Card para integrar a lógica de favoritos e atualiza a página Favoritos para utilizar o contexto de favoritos

--- React/cinetag/src/components/Card/index.js
+++ React/cinetag/src/components/Card/index.js
@@ -1,18 +1,27 @@
-import styles from "./Card.module.css";
+import { useFavoritoContext } from 'contextos/Favoritos';
+import styles from './Card.module.css';
 import iconeFavoritar from './desfavoritar.png';
+import iconeDesfavoritar from './favoritar.png';
+
 
 function Card({ id, titulo, capa }) {
-  return (
-    <div className={styles.container}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
-      <img
-        src={iconeFavoritar}
-        alt="Favoritar filme"
-        className={styles.favoritar}
-      />
-    </div>
-  );
+    console.log(id, titulo, capa)
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+    return (
+        <div className={styles.container}>
+            <img src={capa} alt={titulo} className={styles.capa} />
+            <h2>{titulo}</h2>
+            <img src={icone}
+                alt="Favoritar filme"
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }} />
+        </div>
+
+    )
 }
 
 export default Card;
