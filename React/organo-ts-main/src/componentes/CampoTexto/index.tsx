e1213f18fc8261bcdd5d43ec43a68ca798d50884
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-01-21 21:30:20 -0300

    feat: adiciona tipagem TypeScript ao componente CampoTexto e refatora o código

--- React/organo-ts-main/src/componentes/CampoTexto/index.tsx
+++ React/organo-ts-main/src/componentes/CampoTexto/index.tsx
@@ -1,25 +1,31 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 interface CampoTextoProps {
-    
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio: boolean;
 }
 
-const CampoTexto = (props) => {
+const CampoTexto = (props: CampoTextoProps) => {
+  const placeholderModificada = `${props.placeholder}...`;
 
-    const placeholderModificada = `${props.placeholder}...` 
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    props.aoAlterado(evento.target.value);
+  };
 
-    const aoDigitado = (evento) => {
-        props.aoAlterado(evento.target.value)
-    }
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div className="campo-texto">
-            <label>
-                {props.label}
-            </label>
-            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
