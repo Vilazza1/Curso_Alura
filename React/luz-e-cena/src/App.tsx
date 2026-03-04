23f4585767cceb4356cc910d958e5eb7c13a0872
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-03-03 22:49:03 -0300

    Componente Link

--- React/luz-e-cena/src/App.tsx
+++ React/luz-e-cena/src/App.tsx
@@ -1,8 +1,12 @@
 import Banner from "./components/Banner";
+import Link from "./components/Link";
 
 function App() {
   return (
-    <Banner src="public/Imagens/Banner principal/Banner.png" alt="Banner" />
+    <>
+      <Banner src="public/Imagens/Banner principal/Banner.png" alt="Banner" />
+      <Link href="/" target="_blanck">Link</Link>
+    </>
   );
 }
 
