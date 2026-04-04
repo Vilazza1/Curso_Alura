74bddb9ea44710ed48d9c6622fc78f3869444576
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-04-03 23:47:49 -0300

    feat: adiciona componente MovieList com estilos e lógica

---
+++ React/luz-e-cena/src/components/MovieList/index.tsx
@@ -0,0 +1,33 @@
+import CardMovie from "../CardMovie";
+import styles from "./MovieList.module.css";
+
+type Categoria = "2D" | "3D";
+
+type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
+
+interface Movie {
+  id: number;
+  src: string;
+  alt: string;
+  titulo: string;
+  categoria: Categoria;
+  censura: Censura;
+  genero: string;
+  duracao: number;
+}
+
+interface MovieListProps {
+  movies: Movie[];
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+  return (
+    <ul className={styles.lista}>
+      {movies.map((movie) => (
+        <CardMovie key={movie.id} {...movie} />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
