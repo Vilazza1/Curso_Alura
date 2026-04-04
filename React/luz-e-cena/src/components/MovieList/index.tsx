2be833aff08963a60341a0de924aebb9006ea07c
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-04-04 00:09:13 -0300

    feat: adiciona tipos e componentes Fieldset e Tag, refatora CardMovie e MovieList

--- React/luz-e-cena/src/components/MovieList/index.tsx
+++ React/luz-e-cena/src/components/MovieList/index.tsx
@@ -1,20 +1,7 @@
+import type { Movie } from "../../types";
 import CardMovie from "../CardMovie";
 import styles from "./MovieList.module.css";
 
-type Categoria = "2D" | "3D";
-
-type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
-
-interface Movie {
-  id: number;
-  src: string;
-  alt: string;
-  titulo: string;
-  categoria: Categoria;
-  censura: Censura;
-  genero: string;
-  duracao: number;
-}
 
 interface MovieListProps {
   movies: Movie[];
