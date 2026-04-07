fac63e2a28d8cc01c925f907ef0ce6a225e55d8e
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-04-06 22:06:50 -0300

    feat: adiciona hook useFilterMovies para gerenciar a filtragem de filmes e integra ao componente MovieSection

--- React/luz-e-cena/src/components/MovieSection/index.tsx
+++ React/luz-e-cena/src/components/MovieSection/index.tsx
@@ -5,21 +5,32 @@ import InputText from "../InputText";
 import styles from "./MovieSection.module.css";
 import MovieList from "../MovieList";
 import useFetchMovies from "../hooks/useFetchMovies";
+import useFilterMovies from "../hooks/useFilterMovies";
 
 const MovieSection = () => {
   const { movies, erro, isLoading } = useFetchMovies();
+  const { searchTerm, handleSearch, setSearchTerm, filteredMovies } =
+    useFilterMovies(movies);
 
   return (
     <main>
       <section className={styles.container}>
         <Fieldset variant="secondary">
-          <InputText placeholder="Buscar Fimes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(
+              event: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+            ) => setSearchTerm(event.target.value)}
+            placeholder="Buscar Fimes..."
+          />
+          <Button variant="icon" onClick={handleSearch}>
             <FaSearch />
           </Button>
         </Fieldset>
         <h1 className={styles.titulo}>Em Cartas</h1>
-        <MovieList movies={movies} />
+        {isLoading && <p>Carregando Filmes</p>}
+        {erro && <p className={styles.error}>{erro}</p>}
+        <MovieList movies={filteredMovies} />
       </section>
     </main>
   );
