ddf907c6f2d082ce3b01969fea56a6ace1777cca
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-04-07 21:16:25 -0300

    feat: adiciona componente Footer com estilos e informações

--- React/luz-e-cena/src/App.tsx
+++ React/luz-e-cena/src/App.tsx
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieSection from "./components/MovieSection";
 import Newsletter from "./components/Newsletter";
@@ -14,6 +15,7 @@ function App() {
         alt="Banner"
       />
       <Newsletter />
+      <Footer />
     </>
   );
 }
---
+++ React/luz-e-cena/src/components/Footer/FotterInfo.tsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+const FotterInfo = () => {
+  return (
+    <div className={styles.funcionamento}>
+      <h4 className={styles.titulo}>Funcionamento</h4>
+      <span>Segunda a Sexta - 16h as 00h</span>
+      <span>Vinicius.Vilaca2024@hotmail.com</span>
+    </div>
+  );
+};
+
+export default FotterInfo;
