b997f4151d239f5e7bcf8b52f543bc4adae37902
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-05-05 22:13:31 -0300

    feat: implementa saudação dinâmica para o usuário com dados da API

--- React/poupapp/src/componentes/SaudacaoUsuario/index.tsx
+++ React/poupapp/src/componentes/SaudacaoUsuario/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { IUsuario } from "../../types";
+import { obterUsuario } from "../../api";
 
 export const StyledUsuario = styled.div`
   grid-area: usuario;
@@ -14,9 +17,20 @@ export const StyledUsuario = styled.div`
 `;
 
 const SaudacaoUsuario = () => {
+  const [usuraio, setUsuario] = useState<IUsuario | null>(null);
+
+  const buscarUsuario = async () => {
+    const response = await obterUsuario();
+    setUsuario(response[0]);
+  };
+
+  useEffect(() => {
+    buscarUsuario();
+  });
+
   return (
     <StyledUsuario>
-      <h1>Olá, Neilton</h1>
+      <h1>Olá, {usuraio?.nome}</h1>
       <p>Veja como estão suas finanças hoje.</p>
     </StyledUsuario>
   );
