13396f65f24e4376e5c81f457c892a1fc4f9c81c
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-05-25 21:47:23 -0300

    feat: atualiza criação de usuário para incluir cálculo de orçamento diário e ajusta tipos de usuário

--- React/poupapp/src/context/AppContext.tsx
+++ React/poupapp/src/context/AppContext.tsx
@@ -10,7 +10,7 @@ import {
 
 interface AppContextType {
   usuario: IUsuario | null;
-  criaUsuario: (usuario: Omit<IUsuario, "id">) => Promise<void>;
+  criaUsuario: (usuario: Omit<IUsuario, "id" |  "orcamentoDiaro">) => Promise<void>;
   transacoes: ITransacoes[];
   criaTransacao: (
     novaTransacao: Omit<ITransacoes, "id">
@@ -44,7 +44,7 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
     carregaDadosUsuario();
   }, []);
 
-  const criaUsuario = async (usuario: Omit<IUsuario, "id">) => {
+  const criaUsuario = async (usuario: Omit<IUsuario, "id" |  "orcamentoDiaro">) => {
     try {
       const novoUsuario = await criarUsuario(usuario);
       setUsuario(novoUsuario);
